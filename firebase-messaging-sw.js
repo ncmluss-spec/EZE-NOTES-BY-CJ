ecaa929bfce890e3cf735fd26dd938f9816cd4e4
Eze notes by cj <ncmluss@gmail.com>
2026-07-16 22:20:32 +0530

    Initialize Firebase messaging in service worker
    Set up Firebase messaging service worker with notification handling.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCVB2ugk1IxUGd4QHCesg3v0VqcFyxt0c8",
+  authDomain: "eze-notes-by-cj.firebaseapp.com",
+  projectId: "eze-notes-by-cj",
+  storageBucket: "eze-notes-by-cj.firebasestorage.app",
+  messagingSenderId: "156592482475",
+  appId: "1:156592482475:web:5492a1702d4b92e6e1afee"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/logo.png"
+  });
+});
